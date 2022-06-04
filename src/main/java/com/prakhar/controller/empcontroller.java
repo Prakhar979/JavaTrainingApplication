e83340865ce995898e44acd7dc34e879f036fe9d
Prakhar979 <prakharptdr979@gmail.com>
2022-06-04 17:53:00 +0530

    added post mapping

--- src/main/java/com/prakhar/controller/empcontroller.java
+++ src/main/java/com/prakhar/controller/empcontroller.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,7 +25,7 @@ public class empcontroller {
     }
 
     @PostMapping("/add")
-    public String addemp(employee employee){
+    public String addemp(@RequestBody employee employee){
         return empservice.saveemp(employee);
     }
 }
--- src/main/java/com/prakhar/model/employee.java
+++ src/main/java/com/prakhar/model/employee.java
@@ -10,9 +10,24 @@ public class employee {
     private String name;
     private int age;
 
+    public employee() {
+    }
+
     public employee(int id, String name, int age) {
         this.id = id;
         this.name = name;
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
